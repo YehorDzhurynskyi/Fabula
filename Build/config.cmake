9dd891a17c85381bcbacdb051b5b39329dde83b8
Yehor Dzhurynskyi <y.a.dzhurynskyi@gmail.com>
2019-03-31 00:07:50 +0300

    zlib and png cmake modified

--- Build/config.cmake
+++ Build/config.cmake
@@ -3,3 +3,5 @@ if(MSVC) # or other generator platform which support multi-configuration build
 endif()
 
 set(GLM_TEST_ENABLE OFF CACHE BOOL "Generate Unit tests for 'glm' lib")
+set(PNG_BUILD_ZLIB ON CACHE BOOL "Custom zlib Location, else find_package is used")
+set(PNG_TESTS OFF CACHE BOOL "Generate Unit tests for 'png' lib")
